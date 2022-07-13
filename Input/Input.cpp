a7c7230c70120e2b7e634aa8f2b77de92bf985ea
KatoShintaro <k020g1402@g.neec.ac.jp>
2022-07-14 04:03:10 +0900

    マウス処理追加 マウスでワールド空間を指す処理を作成中

--- Input/Input.cpp
+++ Input/Input.cpp
@@ -20,10 +20,18 @@ void Input::Initialize(WinApp* winApp)
 	result = DirectInput8Create(
 		winApp_->GetHInstance(), DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&dinput, nullptr);
 
+	// キーボードデバイスの生成
 	result = dinput->CreateDevice(GUID_SysKeyboard, &devkeyboard, NULL);
 	result = devkeyboard->SetDataFormat(&c_dfDIKeyboard); // 標準形式
 	result = devkeyboard->SetCooperativeLevel(
 		winApp_->GetHwnd(), DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+
+	// マウスデバイスの生成	
+	result = dinput->CreateDevice(GUID_SysMouse, &devMouse, NULL);
+	result = devMouse->SetDataFormat(&c_dfDIMouse2); // 標準形式
+	result = devMouse->SetCooperativeLevel(
+		winApp_->GetHwnd(), DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+
 }
 
 void Input::Update()
@@ -34,9 +42,15 @@ void Input::Update()
 	result = devkeyboard->Acquire();
 	//前回のキー入力を保存
 	memcpy(keyPre, key, sizeof(key));
-
 	//全キーの入力情報を取得
 	result = devkeyboard->GetDeviceState(sizeof(key), key);
+
+	//マウス情報の取得開始
+	result = devMouse->Acquire();
+	//前回の入力を保存
+	mouseStatePre = mouseState;
+	//マウスの入力情報を取得
+	result = devMouse->GetDeviceState(sizeof(mouseState), &mouseState);
 }
 
 bool Input::PushKey(BYTE keyNumber)
@@ -58,4 +72,46 @@ bool Input::TriggerKey(BYTE keyNumber)
 		return true;
 	}
 	return false;
+}
+
+bool Input::PushMouse(MouseButton button)
+{
+	// 0でなければ押している
+	if (mouseState.rgbButtons[button]) {
+		return true;
+	}
+
+	// 押していない
+	return false;
+}
+
+bool Input::TriggerMouse(MouseButton button)
+{
+	// 前回が0で、今回が0でなければトリガー
+	if (!mouseStatePre.rgbButtons[button] && mouseState.rgbButtons[button]) {
+		return true;
+	}
+
+	// トリガーでない
+	return false;
+}
+
+XMFLOAT2 Input::GetMousePos2()
+{
+	POINT p;
+
+	GetCursorPos(&p);
+
+	ScreenToClient(winApp_->GetHwnd(), &p);
+
+	return XMFLOAT2({ p.x,p.y });
+}
+
+XMFLOAT3 Input::GetMousePos3(XMMATRIX viewProjectionMat)
+{
+	XMFLOAT2 p = GetMousePos2();
+
+	XMMATRIX invVPMat = XMMatrixInverse(&XMVECTOR({1,1,1}), viewProjectionMat);
+
+	return XMFLOAT3();
 }
--- Input/Input.h
+++ Input/Input.h
@@ -3,10 +3,26 @@
 #include <Windows.h>
 #define DIRECTINPUT_VERSION		0x0800 // DirectInputのバージョン指定
 #include <dinput.h>
+#include <DirectXMath.h>
 #include "WinApp.h"
+
+using namespace DirectX;
+
+enum MouseButton
+{
+	MOUSE_LEFT,
+	MOUSE_RIGHT,
+	MOUSE_MIDDLE,
+};
 
 class Input
 {
+private:
+	struct MouseMove {
+		LONG    lX;
+		LONG    lY;
+		LONG    lZ;
+	};
 
 public:
 	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
@@ -18,6 +34,11 @@ private:
 	ComPtr<IDirectInputDevice8> devkeyboard = nullptr;
 	ComPtr<IDirectInput8> dinput = nullptr;
 
+	//マウスのデバイス
+	ComPtr<IDirectInputDevice8> devMouse;
+	DIMOUSESTATE2 mouseState = {};
+	DIMOUSESTATE2 mouseStatePre = {};
+
 	WinApp* winApp_ = nullptr;
 
 public:
@@ -26,4 +47,8 @@ public:
 	void Update();
 	bool PushKey(BYTE keyNumber);
 	bool TriggerKey(BYTE keyNumber);
+	bool PushMouse(MouseButton button);
+	bool TriggerMouse(MouseButton button);
+	XMFLOAT2 GetMousePos2();
+	XMFLOAT3 GetMousePos3(XMMATRIX viewProjectionMat);
 };
