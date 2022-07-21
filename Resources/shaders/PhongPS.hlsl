b04c975f83b2ac1328dc4e77726bb71c07ab49f4
KatoShintaro <k020g1402@g.neec.ac.jp>
2022-07-21 10:57:31 +0900

    コメントを追加

--- Resources/shaders/PhongPS.hlsl
+++ Resources/shaders/PhongPS.hlsl
@@ -9,22 +9,28 @@ float4 main(VSOutput input) : SV_TARGET
     float3 normal = input.normal;
     float3 light = float3(10, 10, 1);
 
-    float intensity = saturate(dot(normalize(normal), light));
-
     // アンビエント
     float4 color = tex.Sample(smp, input.uv);
+    // 暗めにするために0.3を掛けた
     float4 ambient = color * 0.3f;
+    // 透明度を1に戻した
     ambient.a = 1;
 
     // ディフューズ
+    // 光源ベクトルと法線ベクトルの内積を算出し、0.0～1.0の間でクランプ
+    float intensity = saturate(dot(normalize(normal), light));
     float4 diffuse = color * 0.5f * intensity;
 
     // スペキュラー
+    // 視線ベクトルを正規化
     float3 eyeDir = normalize(eyePos - input.svpos);
+    // 光源ベクトルと視線ベクトルの正規化し、法線ベクトルとの内積を算出
+    // これを0.0～1.0の間でクランプ
     intensity = saturate(dot(normalize(normal), normalize(light + eyeDir)));
 
     float4 specular = float4(1, 1, 1, 1) * pow(intensity, 30);
 
+    // アンビエント、ディフューズ、スペキュラーを加算
     float4 ads = ambient + diffuse + specular;
 
     return ads;
--- Resources/shaders/PostEffectPS.hlsl
+++ Resources/shaders/PostEffectPS.hlsl
@@ -7,7 +7,7 @@ float4 main(VSOutput input) : SV_TARGET
 {
     float4 texcolor = tex.Sample(smp, input.uv);
 
-    // UVずらし
+    // UVずらし、タイリング
     if (mode == 4)
     {
         texcolor = tex.Sample(smp, input.uv * float2(2, 2) + float2(0.5f, 0.5f));
@@ -30,18 +30,20 @@ float4 main(VSOutput input) : SV_TARGET
     if (mode == 3)
     {
         // ぼかし
-        float4 color = float4(0,0,0,0);
-        float count = 0;
+        float4 color = float4(0,0,0,0); // 合計値保存用
+        float count = 0; // サンプリングしたピクセルの数をカウント
 
         for (float y = -10; y < 10; y++)
         {
             for (float x = -10; x < 10; x++)
             {
+                //ピクセルの色をサンプリングして合計に加算
                 color += tex.Sample(smp, input.uv + float2(x, y) * 0.001f);
                 count++;
             }
         }
 
+        // 平均値を算出
         texcolor.rgb = color.rgb / count;
     }
 
