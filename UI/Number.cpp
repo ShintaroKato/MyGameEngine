8d67752a1db7bae3b123cec4fb8e0f18b924c44a
KatoShintaro <k020g1402@g.neec.ac.jp>
2022-11-02 13:00:51 +0900

    数値表示用のクラスを追加

---
+++ UI/Number.cpp
@@ -0,0 +1,62 @@
+#include "Number.h"
+
+Number::Number()
+{
+}
+
+Number* Number::Create(SpriteCommon* spriteCommon, UINT texNumber, unsigned int digit_, XMFLOAT2 position, XMFLOAT2 anchorpoint, bool isFlipX, bool isFlipY)
+{
+	Number* instance = new Number();
+
+	instance->Initialize(spriteCommon, texNumber, position, anchorpoint, isFlipX, isFlipY);
+
+	// 桁数設定
+	if (digit_ == 0)
+	{
+		digit_ = 1;
+	}
+	instance->digit = digit_;
+
+	for (int i = 0; i < instance->digit; i++)
+	{
+		instance->numbers.push_back(Sprite::Create(spriteCommon, texNumber, position, anchorpoint, isFlipX, isFlipY));
+	}
+
+	return instance;
+}
+
+void Number::SetSequence(int value, float x, float y, XMFLOAT2 rectSize, int space)
+{
+	// 各位の数字を保存
+	std::vector<char> eachNumber(digit);
+
+	// 表示する数量
+	int number = value;
+
+	// 表示する数量の桁数と一致する10のn乗の数を求める
+	int digit_ = pow(10, digit) / 10;
+
+	for (int i = 0; i < eachNumber.size(); i++)
+	{
+		// 指定した数量を10^nで除算し、一番上の位から求める
+		eachNumber[i] = number / digit_;
+		number = number % digit_;
+		digit_ = digit_ / 10;
+	}
+
+	for (int i = 0; i < eachNumber.size(); i++)
+	{
+		numbers[i]->SetSize(rectSize);
+		numbers[i]->SetTextureRect({ (float)(rectSize.x * eachNumber[i]), 0 }, rectSize);
+		numbers[i]->SetPosition({ (float)(rectSize.x * i) + x + space, y });
+		numbers[i]->Update();
+	}
+}
+
+void Number::Draw()
+{
+	for (int i = 0; i < digit; i++)
+	{
+		numbers[i]->Draw();
+	}
+}
---
+++ UI/Number.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Sprite.h"
+
+class Number : public Sprite
+{
+public:
+
+	Number();
+
+	static Number* Create(SpriteCommon* spriteCommon, UINT texNumber, unsigned int digit, XMFLOAT2 position, XMFLOAT2 anchorpoint,
+		bool isFlipX = false, bool isFlipY = false);
+
+	/// <summary>
+	/// 数列を表示
+	/// </summary>
+	/// <param name="value">数量</param>
+	/// <param name="x">X座標</param>
+	/// <param name="y">Y座標</param>
+	/// <param name="space">文字同士の間隔</param>
+	/// <param name="rectSize">切り取るサイズ</param>
+	void SetSequence(int value, float x, float y, XMFLOAT2 rectSize, int space = 0);
+
+	void Draw();
+
+private:
+	// 桁数
+	unsigned int digit = 1;
+	// 切り分けられた画像を保存する配列
+	std::vector<Sprite*> numbers;
+};
