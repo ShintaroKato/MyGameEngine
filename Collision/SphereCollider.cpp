8550d36d8167820f344339bfc5dd4719395d4093
KatoShintaro <k020g1402@g.neec.ac.jp>
2022-11-10 11:38:57 +0900

    正方形の当たり判定を生成する処理を追加

--- Collision/SphereCollider.cpp
+++ Collision/SphereCollider.cpp
@@ -7,10 +7,23 @@ void SphereCollider::Update()
 	// ワールド行列から座標を抽出
 	XMMATRIX matWorld{};
 
-	if(obj) matWorld = obj->GetMatWorld();
-	if(fbx) matWorld = fbx->GetMatWorld();
+	if (obj) matWorld = obj->GetMatWorld();
+	if (fbx) matWorld = fbx->GetMatWorld();
 
 	// 球のメンバ変数を更新
-	Sphere::center = matWorld.r[3] + offset;
+	Sphere::center = offset;
 	Sphere::radius = radius;
+
+	CreateSquareXZ();
+}
+
+void SphereCollider::CreateSquareXZ()
+{
+	square = {
+		offset, // 中心
+		offset.m128_f32[2] + radius, // 上端
+		offset.m128_f32[2] - radius, // 下端
+		offset.m128_f32[0] - radius, // 左端
+		offset.m128_f32[0] + radius  // 右端
+	};
 }
--- Collision/SphereCollider.h
+++ Collision/SphereCollider.h
@@ -31,7 +31,84 @@ public:
 
 	void Update() override;
 
-	inline void SetRadius(float radius) { this->radius = radius; }
+	inline void SetRadius(float radius) {
+#pragma once
+
+#include "BaseCollider.h"
+#include "CollisionPrimitive.h"
+
+#include <DirectXMath.h>
+
+
+		/// <summary>
+		/// 球衝突判定オブジェクト
+		/// </summary>
+		class SphereCollider :public BaseCollider, public Sphere
+		{
+		private:
+			using XMVECTOR = DirectX::XMVECTOR;
+
+		public:
+			SphereCollider(XMVECTOR offset = { 0,0,0,0 }, float radius = 1.0f, bool squareFlag = false)
+				: offset(offset), radius(radius), isSquareEnabled(squareFlag)
+			{
+				// 球形状をセット
+				if (!isSquareEnabled)
+				{
+					shapeType = COLLISIONSHAPE_SPHERE;
+				}
+				else
+				{
+					shapeType = COLLISIONSHAPE_SPHERE_SQUARE;
+					CreateSquareXZ();
+				}
+			}
+
+			SphereCollider(Sphere sphere, bool squareFlag = false)
+				: offset(sphere.center), radius(sphere.radius), isSquareEnabled(squareFlag)
+			{
+				// 球形状をセット
+				if (!isSquareEnabled)
+				{
+					shapeType = COLLISIONSHAPE_SPHERE;
+				}
+				else
+				{
+					shapeType = COLLISIONSHAPE_SPHERE_SQUARE;
+					CreateSquareXZ();
+				}
+			}
+
+			void Update() override;
+
+			inline void SetRadius(float radius) { this->radius = radius; }
+
+			inline const XMVECTOR& GetOffset() { return offset; }
+
+			inline void SetOffset(const XMVECTOR& offset) { this->offset = offset; }
+
+			inline float GetRadius() { return radius; }
+
+			inline void SetSphere(Sphere sphere)
+			{
+				this->offset = sphere.center;
+				this->radius = sphere.radius;
+			}
+
+			// XZ平面に平行な面を作る
+			inline void CreateSquareXZ();
+
+		private:
+			// オブジェクト中心からのオフセット
+			XMVECTOR offset;
+			// 半径
+			float radius;
+
+			// 面を持たせるか否か
+			bool isSquareEnabled = false;
+		};
+
+		this->radius = radius; }
 
 	inline const XMVECTOR& GetOffset() { return offset; }
 
