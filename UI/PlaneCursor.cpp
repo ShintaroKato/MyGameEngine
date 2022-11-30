07e7d13a80c1f692120a764f37eea68eded548bf
KatoShintaro <k020g1402@g.neec.ac.jp>
2022-12-01 02:27:40 +0900

    ステージオブジェクト操作用のカーソルのクラスを追加

---
+++ UI/PlaneCursor.cpp
@@ -0,0 +1,91 @@
+#include "PlaneCursor.h"
+#include "CollisionManager.h"
+#include "CollisionAttribute.h"
+#include "Collision.h"
+#include "Input.h"
+#include "SceneManager.h"
+
+ObjectOBJ* PlaneCursor::cursor;
+XMFLOAT3 PlaneCursor::pos;
+float PlaneCursor::radius = 5.0f;
+bool PlaneCursor::isDrag;
+bool PlaneCursor::used;
+bool PlaneCursor::isInGame;
+Sphere PlaneCursor::sphere;
+SphereCollider* PlaneCursor::sphereColl;
+
+void PlaneCursor::Initialize(ObjectOBJ* object)
+{
+	cursor = object;
+
+	// コライダーの追加
+	sphere.center = { pos.x, pos.y + radius, pos.z,0 };
+	sphere.radius = radius;
+
+	sphereColl = new SphereCollider(sphere, true);
+
+	SetPosition(cursor->GetPosition());
+}
+
+void PlaneCursor::Update()
+{
+	if (SceneManager::GetScene() != EDIT) return;
+
+	Move();
+
+	SetPosition(pos);
+
+	cursor->Update();
+}
+
+void PlaneCursor::Move()
+{
+	Input* input = Input::GetInstance();
+
+	XMVECTOR vec = input->CursorPoint3D(Camera::GetViewMatrix(), Camera::GetProjectionMatrix());
+	Ray ray;
+	ray.start = input->CalcScreenToWorld(input->GetMousePos2(), 0.0f, Camera::GetViewMatrix(), Camera::GetProjectionMatrix());
+	ray.dir = input->CalcScreenToWorld(input->GetMousePos2(), 1.0f, Camera::GetViewMatrix(), Camera::GetProjectionMatrix());
+	ray.dir = XMVector3Normalize(ray.dir - ray.start);
+	float distance = 0;
+	XMVECTOR inter{};
+
+	pos = { vec.m128_f32[0], 0, vec.m128_f32[2] };
+	sphere.center = { pos.x, pos.y, pos.z };
+	sphereColl->SetOffset(sphere.center);
+
+	// 動かせる範囲
+	if (pos.x < -60) pos.x = -60;
+	if (pos.x > 60) pos.x = 60;
+
+	if (pos.z < -60) pos.z = -60;
+	if (pos.z > 60) pos.z = 60;
+
+	if (pos.y > 0) pos.y = 0;
+}
+
+void PlaneCursor::Draw()
+{
+	if (SceneManager::GetScene() != EDIT) return;
+
+	cursor->Draw();
+}
+
+void PlaneCursor::SetPosition(const XMFLOAT3& position)
+{
+	pos = position;
+	pos.y = 0.0f;
+
+	if (!isDrag) radius = 5.0f;
+
+	// コライダーの追加
+	sphere.center = { pos.x, pos.y, pos.z,0 };
+	sphere.radius = radius;
+
+	*sphereColl = SphereCollider(sphere, true);
+	sphereColl->SetAttribute(COLLISION_ATTR_PLANE_CURSOR);
+
+	cursor->SetScale({ radius * 2, 1.0f, radius * 2 });
+	cursor->SetPosition(pos);
+	cursor->SetCollider(sphereColl);
+}
---
+++ UI/PlaneCursor.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "ObjectOBJ.h"
+#include "SphereCollider.h"
+
+class PlaneCursor
+{
+private: // エイリアス
+	// Microsoft::WRL::を省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMVECTOR = DirectX::XMVECTOR;
+	using XMMATRIX = DirectX::XMMATRIX;
+
+public:
+	PlaneCursor(){}
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <returns>成否</returns>
+	static void Initialize(ObjectOBJ* object);
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	static void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	static void Draw();
+
+	/// <summary>
+	/// オブジェクトを掴んでいるか判定するフラグをセット
+	/// </summary>
+	static void SetIsDrag(bool isDrag) { PlaneCursor::isDrag = isDrag; }
+
+	static bool GetIsDrag() { return isDrag; }
+
+	static void SetRadius(float radius) { PlaneCursor::radius = radius; }
+
+	/// <summary>
+	/// 移動
+	/// </summary>
+	static void Move();
+
+	static XMFLOAT3 GetPosition() { return pos; }
+
+	static void SetPosition(const XMFLOAT3& position);
+
+	static void SetColor(const XMFLOAT4& color) { cursor->SetColor(color); }
+
+private:
+	static ObjectOBJ* cursor;
+	// 座標
+	static XMFLOAT3 pos;
+	// 半径
+	static float radius;
+	// 掴まれているか否か
+	static bool isDrag;
+	// 使用されているか否か
+	static bool used;
+	// ゲーム本編か否か
+	static bool isInGame;
+	// コライダー
+	static Sphere sphere;
+	static SphereCollider* sphereColl;
+};
